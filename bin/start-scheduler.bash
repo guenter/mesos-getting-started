cf37f6d446ec8d029812bf38d8553bf5b794bd01
Tobi Knaup <tobi@knaup.me>
2013-08-06 21:51:37 -0500

    Fix startup script

--- bin/start-scheduler.bash
+++ bin/start-scheduler.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Change this to point to your local Mesos installation
-MESOS_HOME=/Users/tobi/code/incubator-mesos
+if [ -z ${MESOS_HOME+x} ]
+then
+  echo "MESOS_HOME is not set. Defaulting to /usr/local"
+  MESOS_HOME='/usr/local'
+else
+  echo "MESOS_HOME is set to: $MESOS_HOME"
+fi
+
+export MESOS_NATIVE_LIBRARY=$(find "$MESOS_HOME" -name libmesos.dylib -or -name libmesos.so | head -n1)
+echo "MESOS_NATIVE_LIBRARY set to $MESOS_NATIVE_LIBRARY"
 
 FRAMEWORK_HOME=$(dirname $0)/../
-
-export MESOS_NATIVE_LIBRARY=$(find $MESOS_HOME -name "libmesos.dylib" | head -n1)
-
-echo "FRAMEWORK_HOME is set to $FRAMEWORK_HOME"
-echo "MESOS_HOME is set to $MESOS_HOME"
-echo "MESOS_NATIVE_LIBRARY is set to $MESOS_NATIVE_LIBRARY"
 
 # Start the scheduler
 java -cp $FRAMEWORK_HOME/target/mesos-getting-started-*.jar mesosphere.mesos_scala.Main $@
